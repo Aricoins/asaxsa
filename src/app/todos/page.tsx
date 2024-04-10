cca47e5b05dad273aca6b80bc3148978ef4cf611
Aricoins <arielgarcia79@gmail.com>
2024-04-10 00:45:51 -0300

    estilos

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Input, Button, Select, DatePicker, Row, Col, Modal } from 'antd';
+import { FloatButton, Input, Button, Select, DatePicker, Row, Col, Modal } from 'antd';
 import './todos.module.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+
 
 const { Option } = Select;
 const URL = "https://servertodo-production.up.railway.app/api/todos/";
@@ -60,7 +61,7 @@ Aos.init();
 
   return (
     <>
-      {loading ? (
+      {<FloatButton/> ? (
         <div className="todo-container">
           <h1 data-aos="fade-left" style={{ justifyContent: "center", textAlign: "center", color: "rgb(227, 156, 162)" }}>ToDo List</h1>
           <Row gutter={16} className="mb-4">
