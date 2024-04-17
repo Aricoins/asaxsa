0eb895a0eb6b5bb48d776940b63078481d37a7cb
Ariel G Rogel <95644790+Aricoins@users.noreply.github.com>
2024-04-17 19:59:39 -0300

    Update page.tsx

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Button, Input, Select, DatePicker, Row, Col, Modal, Table, Tag, Space, notification } from 'antd';
